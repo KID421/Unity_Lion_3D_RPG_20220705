efdd912fff02926522aefbf66d7841b9740b0656
KID <50666207+KID421@users.noreply.github.com>
2022-07-19 12:03:52 +0800

    Update 角色移動與旋轉

--- Unity_Lion_3D_RPG_20220705/Assets/Scripts/ThirdPersonController.cs
+++ Unity_Lion_3D_RPG_20220705/Assets/Scripts/ThirdPersonController.cs
@@ -18,6 +18,8 @@ namespace KID
 
         private Animator ani;
         private CharacterController controller;
+        private Vector3 direction;
+        private Transform traCamera;
         #endregion
 
         #region 事件
@@ -25,6 +27,15 @@ namespace KID
         {
             ani = GetComponent<Animator>();
             controller = GetComponent<CharacterController>();
+
+            // 透過名稱搜尋物件，建議放在 Awake、Start 或者執行一次的架構內
+            // traCamera = GameObject.Find("Main Camera").GetComponent<Transform>();
+            traCamera = GameObject.Find("Main Camera").transform;
+        }
+
+        private void Update()
+        {
+            Move();
         }
         #endregion
 
@@ -34,7 +45,24 @@ namespace KID
         /// </summary>
         private void Move()
         {
+            float v = Input.GetAxisRaw("Vertical");     // 取得前後按鍵值：WS、↑↓
+            float h = Input.GetAxisRaw("Horizontal");   // 取得左右按鍵值：AD、←→
 
+            // print("<color=yellow>垂直軸向：" + v + "</color>");
+
+            #region 旋轉
+            // transform.rotation = traCamera.rotation; // 沒有過渡
+
+            // 玩家角度 = 四元數.插值(玩家角度，攝影機角度，速度 * 每幀時間)
+            transform.rotation = Quaternion.Lerp(transform.rotation, traCamera.rotation, turn * Time.deltaTime);
+            #endregion
+
+            direction.z = v;                            // 物件前後軸為 Z 軸：指定為 v 前後按鍵
+            direction.x = h;                            // 物件左右軸為 X 軸：指定為 h 左右按鍵
+
+            // 角色控制器.移動(方向 * 速度)
+            // Time.deltaTime 每幀的時間
+            controller.Move(direction * speed * Time.deltaTime);
         }
 
         /// <summary>
