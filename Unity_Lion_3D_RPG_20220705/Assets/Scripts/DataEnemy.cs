279154350c5fe8d1de9ed1238a7b71ee3772255a
KID <50666207+KID421@users.noreply.github.com>
2022-08-15 17:14:55 +0800

    Update 敵人 AI

--- Unity_Lion_3D_RPG_20220705/Assets/Scripts/DataEnemy.cs
+++ Unity_Lion_3D_RPG_20220705/Assets/Scripts/DataEnemy.cs
@@ -19,9 +19,15 @@ namespace KID
         [Header("走路速度"), Range(0, 100)]
         public float speedWalk;
         [Header("掉落道具機率"), Range(0, 1)]
-        public float propbilityProp;
+        public float probabilityProp;
         [Header("掉落道具")]
         public GameObject goProp;
+        [Header("等待時間範圍")]
+        public Vector2 timeIdleRange;
+        [Header("要追蹤的目標圖層")]
+        public LayerMask layerTarget;
+        [Header("攻擊間隔"), Range(0, 5)]
+        public float intervalAttack;
     }
 }
 
